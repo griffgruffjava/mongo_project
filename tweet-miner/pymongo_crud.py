e58cde33c03ce5bdb1618c32aeb1cc71b2341f2e
griffgruffjava <C_Train@Macintosh.local>
2016-11-04 08:59:25 +0000

    investigating why api stopped working

--- tweet-miner/pymongo_crud.py
+++ tweet-miner/pymongo_crud.py
@@ -9,48 +9,48 @@ tweets = db['tweets']
 
 # CREATE methods - single and multi
 
-
-def create_tweet(tweet):
-    tweet = tweets.insert_one(tweet)
-    return tweet
-
-
-
-def multi_create_tweet(multi_tweet):
-    created_tweets = tweets.insert_many(multi_tweet)
-    return created_tweets
-
-#  READ methods
-
-
-def get_tweet_by_id(id):
-    print tweets.count()
-    return tweets.find_one({'_id': ObjectId(id)})
-
-
-def get_first_tweet(key, value):
-        found_tweet = tweets.find_one({key: value})
-        return found_tweet
-
-
-def get_all():
-    return tweets.find()
-
-
-# Update
-
-
-def update_tweet(id, key, value):
-    return tweets.find_one_and_update({'_id': ObjectId(id)}, {'$set': {key: value}}, return_document=ReturnDocument.AFTER)
-
-# Delete
-
-
-def delete_tweet(mongo_id):
-    return tweets.find_one_and_delete({'_id': ObjectId(mongo_id)})
-
-data1 = {'name': 'Mongo_God', 'text': 'On the 8th day I made a tweet'}
-data2 = [data1, {'name': 'Mongo_God', 'text': 'more tweeting here'}]
+#
+# def create_tweet(tweet):
+#     tweet = tweets.insert_one(tweet)
+#     return tweet
+#
+#
+#
+# def multi_create_tweet(multi_tweet):
+#     created_tweets = tweets.insert_many(multi_tweet)
+#     return created_tweets
+#
+# #  READ methods
+#
+#
+# def get_tweet_by_id(id):
+#     print tweets.count()
+#     return tweets.find_one({'_id': ObjectId(id)})
+#
+#
+# def get_first_tweet(key, value):
+#         found_tweet = tweets.find_one({key: value})
+#         return found_tweet
+#
+#
+# def get_all():
+#     return tweets.find()
+#
+#
+# # Update
+#
+#
+# def update_tweet(id, key, value):
+#     return tweets.find_one_and_update({'_id': ObjectId(id)}, {'$set': {key: value}}, return_document=ReturnDocument.AFTER)
+#
+# # Delete
+#
+#
+# def delete_tweet(mongo_id):
+#     return tweets.find_one_and_delete({'_id': ObjectId(mongo_id)})
+#
+# data1 = {'name': 'Mongo_God', 'text': 'On the 8th day I made a tweet'}
+# data2 = [data1, {'name': 'Mongo_God', 'text': 'more tweeting here'}]
 
 # print create_tweet(data1)
 
